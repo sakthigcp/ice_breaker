f9589b55bbcb08e4bbc0cfed2da0a775236affa6
Sakthivel Balasubramanian <sakthib1@in.ibm.com>
2025-02-07 10:28:19 +0530

    updated icr_breaker program

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,51 +1,33 @@
-from typing import Tuple
-from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
-from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
-from chains.custom_chains import (
-    get_summary_chain,
-    get_interests_chain,
-    get_ice_breaker_chain,
-)
-from third_parties.linkedin import scrape_linkedin_profile
-from third_parties.twitter import scrape_user_tweets, scrape_user_tweets_mock
-from output_parsers import (
-    Summary,
-    IceBreaker,
-    TopicOfInterest,
-)
+import os
+from dotenv import load_dotenv
 
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain 
 
-def ice_break_with(
-    name: str,
-) -> Tuple[Summary, TopicOfInterest, IceBreaker, str]:
-    linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+information = """
+Arvind Krishna (born November 23, 1962)[3] is an Indian-American business executive, and the chairman and CEO of IBM. 
+He has been CEO of IBM since April 2020 and chairman since January 2021.[4][5] 
+Krishna began his career at IBM in 1990, at its Thomas J. Watson Research Center,[6] and was promoted to senior vice president in 2015, managing IBM Cloud & Cognitive Software and IBM Research divisions. He was a principal architect of the acquisition of Red Hat, the largest acquisition in the company’s history.[7][8]]
+"""
 
-    twitter_username = twitter_lookup_agent(name=name)
-    tweets = scrape_user_tweets_mock(username=twitter_username)
-
-    summary_chain = get_summary_chain()
-    summary_and_facts: Summary = summary_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets},
-    )
-
-    interests_chain = get_interests_chain()
-    interests: TopicOfInterest = interests_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets}
-    )
-
-    ice_breaker_chain = get_ice_breaker_chain()
-    ice_breakers: IceBreaker = ice_breaker_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets}
-    )
-
-    return (
-        summary_and_facts,
-        interests,
-        ice_breakers,
-        linkedin_data.get("photoUrl"),
-    )
-
-
+load_dotenv()
 if __name__ == "__main__":
-    pass
+    print("Hello LangChain!")
+    load_dotenv()
+    print(os.environ.get('OPENAI_API_KEY'))
+    print(os.getenv('OPENAI_API_KEY'))
+    print(os.environ['OPENAI_API_KEY'])
+    # This is in github - check there in the github page
+
+    summary_template = """given the information {information} about a person from I want you to create:
+    1. a short summary
+    2. two interesting facts about them"""
+
+    summary_prompt_template = PromptTemplate(input_variables=["information"],template=summary_template)
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    chain = summary_prompt_template | llm
+    res = chain.invoke(input={"information":information})
+
+    print(res)
--- test.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-    print("Hello LangChain!")
-    # This is in github - check there
