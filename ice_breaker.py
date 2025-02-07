04a181f1e3738630f29a8d327077f9822006ddef
Sakthivel Balasubramanian <sakthib1@in.ibm.com>
2025-02-07 10:55:56 +0530

    reformat using black

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain 
+from langchain.chains import LLMChain
 
 information = """
 Arvind Krishna (born November 23, 1962)[3] is an Indian-American business executive, and the chairman and CEO of IBM. 
@@ -15,19 +15,21 @@ load_dotenv()
 if __name__ == "__main__":
     print("Hello LangChain!")
     load_dotenv()
-    print(os.environ.get('OPENAI_API_KEY'))
-    print(os.getenv('OPENAI_API_KEY'))
-    print(os.environ['OPENAI_API_KEY'])
+    print(os.environ.get("OPENAI_API_KEY"))
+    print(os.getenv("OPENAI_API_KEY"))
+    print(os.environ["OPENAI_API_KEY"])
     # This is in github - check there in the github page
 
     summary_template = """given the information {information} about a person from I want you to create:
     1. a short summary
     2. two interesting facts about them"""
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"],template=summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
 
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
     chain = summary_prompt_template | llm
-    res = chain.invoke(input={"information":information})
+    res = chain.invoke(input={"information": information})
 
     print(res)
